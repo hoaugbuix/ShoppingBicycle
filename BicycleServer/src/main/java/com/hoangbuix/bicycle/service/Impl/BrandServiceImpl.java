aeec7f99bdd9566dbc59ce5fe6c822c037bf034d
Bui Van Hoang <hoangbui@HSVINA.LOCAL>
2021-12-02 07:33:21 +0700

    upaddead

--- BicycleServer/src/main/java/com/hoangbuix/bicycle/service/Impl/BrandServiceImpl.java
+++ BicycleServer/src/main/java/com/hoangbuix/bicycle/service/Impl/BrandServiceImpl.java
@@ -1,16 +1,27 @@
 package com.hoangbuix.bicycle.service.Impl;
 
+import com.hoangbuix.bicycle.dao.BrandDAO;
 import com.hoangbuix.bicycle.entity.BrandEntity;
 import com.hoangbuix.bicycle.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class BrandServiceImpl implements BrandService {
+    @Autowired
+    private BrandDAO<BrandEntity> brandDAO;
+
     @Override
-    public int save(BrandEntity instance) {
-        return 0;
+    public int save(BrandEntity brand) {
+        int id = 0;
+        try {
+            id = brandDAO.save(brand);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return id;
     }
 
     @Override
